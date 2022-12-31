c2084ba21da7e4abf12029b2a69b3b6c8e5a7bf3
Rahwa Yohannes <rahwahaile578@gmail.com>
2022-12-31 11:11:41 +0300

    authentication/authorization functionality

--- middlewares/errorHandlerMiddleware.js
+++ middlewares/errorHandlerMiddleware.js
@@ -2,7 +2,7 @@ const CustomAPIError  = require("../errors/custom-error")
 
 const errorHandlerMiddleware = (err, req, res, next)=>{
     if(err instanceof CustomAPIError){
-        res.status(err.statusCode).json({ msg: err.message })
+       return res.status(err.statusCode).json({ msg: err.message })
     }
     res.status(500).send('Something is wrong, please try again later')
 }
